fca27f168478b8014402fb8626b9848c8c277ea3
shift1425 <shift1425@gmail.com>
2020-10-23 16:43:54 -0400

    a

--- public/scripts/get-location.js
+++ public/scripts/get-location.js
@@ -1,6 +1,6 @@
 navigator.geolocation.watchPosition((position) => {
     localStorage.setItem('latitude', position.coords.latitude);
-    localStorage.setItem('longitude', position.coords.longitude);       
+    localStorage.setItem('longitude', position.coords.longitude); 
    }, (err) => {
       alert("A aplicação Precisa acessar seu geolocalização, por favor permita");
 });
@@ -8,12 +8,11 @@ navigator.geolocation.watchPosition((position) => {
 latitude = localStorage.getItem('latitude');
 longitude = localStorage.getItem('longitude');
 
-while (!latitude) {
-    fetch(`https://nominatim.openstreetmap.org/reverse?&format=json&lat=${latitude}&lon=${longitude}`).then( async (response)=>{
-    
+fetch(`https://nominatim.openstreetmap.org/reverse?&format=json&lat=${latitude}&lon=${longitude}`).then( async (response)=>{
+
     positionUser = await response.json()
     document.getElementById('state').innerHTML = positionUser.address.state
     document.getElementById('city').innerHTML = positionUser.address.city
 })
-setTimeout(500)
-}
+    
+    

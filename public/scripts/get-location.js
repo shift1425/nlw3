3d95c397bd696671af059c3514d59cdb0a339952
shift1425 <shift1425@gmail.com>
2020-10-23 17:17:40 -0400

    a

--- public/scripts/get-location.js
+++ public/scripts/get-location.js
@@ -8,11 +8,13 @@ navigator.geolocation.watchPosition((position) => {
 latitude = localStorage.getItem('latitude');
 longitude = localStorage.getItem('longitude');
 
-fetch(`https://nominatim.openstreetmap.org/reverse?&format=json&lat=${latitude}&lon=${longitude}`).then( async (response)=>{
+fetch(`https://revgeocode.search.hereapi.com/v1/revgeocode?at=${latitude},${longitude}&resultType=address&lang=pt-BR&apiKey=py5AJcABmfDQ7NJ9dBFhSTUzZs4vJg9_iJ3sFGSwQP4`).then( async (response)=>{
 
     positionUser = await response.json()
-    document.getElementById('state').innerHTML = positionUser.address.state
-    document.getElementById('city').innerHTML = positionUser.address.city
+    const a = positionUser.items[0].address
+    console.log(a)
+    document.getElementById('state').innerHTML = positionUser.items.address.state
+    document.getElementById('city').innerHTML = positionUser.items.address.city
 })
     
     

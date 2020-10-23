ad42b2747d6d348a081d98f2596887dcedcfd510
shift1425 <shift1425@gmail.com>
2020-10-23 16:26:28 -0400

    update

--- public/scripts/get-location.js
+++ public/scripts/get-location.js
@@ -7,9 +7,13 @@ navigator.geolocation.watchPosition((position) => {
 
 latitude = localStorage.getItem('latitude');
 longitude = localStorage.getItem('longitude');
-fetch(`https://nominatim.openstreetmap.org/reverse?&format=json&lat=${latitude}&lon=${longitude}`).then( async (response)=>{
+
+while (!latitude) {
+    fetch(`https://nominatim.openstreetmap.org/reverse?&format=json&lat=${latitude}&lon=${longitude}`).then( async (response)=>{
     
     positionUser = await response.json()
     document.getElementById('state').innerHTML = positionUser.address.state
     document.getElementById('city').innerHTML = positionUser.address.city
 })
+setTimeout(500)
+}
